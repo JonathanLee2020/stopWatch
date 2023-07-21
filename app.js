cc80eea0719e872a9a03ac1c7e445e03fefb0009
Jonathan Lee <jonathanlee882@gmail.com>
2023-07-21 16:08:08 +1000

    added reset functionality

--- app.js
+++ app.js
@@ -30,14 +30,25 @@ const $minutes = document.getElementsByClassName("timer__minutes")[0];
 console.log($millis.length, $seconds, $minutes);
 let startTime;
 let pausedTime = 0; 
+let currentlyRunning = false;
+let alreadyStopped = true;
+let alreadyReset = true;
 function startTimer () {
+    alreadyReset = false;
+    if (currentlyRunning == true) {
+        alert("timer already running, click reset to restart timer");
+        return;
+    }
     startTime = Date.now();
+    alreadyStopped = false;
+    currentlyRunning = true;
     requestAnimationFrame(updateTimer)
 }
 
 function updateTimer () {
     // 
-    timeElapsed = pausedTime + Date.now() - startTime
+    // timeElapsed = pausedTime + Date.now() - startTime;
+    timeElapsed = Date.now() - startTime + pausedTime;
     let millis = timeElapsed % 1000;
     millis = millis + "";
     const millisText = millis.padStart(3, 0);
@@ -58,14 +69,38 @@ function updateTimer () {
 }
 
 function stopTimer () {
-    console.log("here we are sage mode naruto");
+    // pausedTime is the previosu paused time plus the amount of time since hte last pause
+    if (alreadyStopped == true) {
+        alert("timer has already been stopped genius");
+        return;
+    }
+    alreadyStopped = true;
     pausedTime = pausedTime + Date.now() - startTime;
+    if (pausedTime == 0) {
+        alert("can't stop a timer that hasn't been started yet sir");
+        return;
+    }
+    currentlyRunning = false;
     cancelAnimationFrame(cancelId)
 }
 
 function resetTimer () {
-
+    if (alreadyReset) {
+        console.log("repelled")
+        return;
+    }
+    cancelAnimationFrame(cancelId);
+    alreadyReset = true;
+    $millis.innerHTML = "000";
+    $seconds.innerHTML = "00";
+    $minutes.innerHTML = "00";
+    startTime = 0;
+    pausedTime = 0;
+    currentlyRunning = false;
+    alreadyStopped = true;
 }
+
+
 
 console.log(Date.now());
 let currTime = Date.now();
